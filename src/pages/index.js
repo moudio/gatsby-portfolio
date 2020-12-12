06a9924005f0679b0bb03f647386adee86a53467
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-12 08:33:31 +0000

    Correct the mobile nav overlay to take the full height

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,4 +1,5 @@
 import React from "react"
+import { Helmet } from "react-helmet"
 import About from "../Components/About/About"
 import CTA from "../Components/CTA/CTA"
 import Hero from "../Components/Hero/Hero"
@@ -11,6 +12,10 @@ import "./index.scss"
 export default function Home() {
   return (
     <Layout>
+      <Helmet>
+        <meta charSet="utf-8" />
+        <title>Mouhamadou | Full-Stack Developer</title>
+      </Helmet>
       <div className={HomeStyles.app}>
         <div className={HomeStyles.appContainer}>
           <Hero />
