7a13eaef550c4026704c010364f5ab5a40034a99
Mouhamadou <mouhamadoudioufsn@gmail.com>
2020-12-13 11:01:35 +0000

    Add fade in animation for components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,30 +1,54 @@
 import React from "react"
+import { useState } from "react"
+import { useEffect } from "react"
 import { Helmet } from "react-helmet"
 import About from "../Components/About/About"
+
 import CTA from "../Components/CTA/CTA"
 import Hero from "../Components/Hero/Hero"
 import Layout from "../Components/Layout"
 import MoreProjects from "../Components/MoreProjects/MoreProjects"
 import Portfolio from "../Components/Portfolio/Portfolio"
 import HomeStyles from "./index.module.scss"
+import balls from "../Images/balls.gif"
 import "./index.scss"
+import FadeIn from "../Components/FadeIn"
 
 export default function Home() {
-  return (
-    <Layout>
-      <Helmet>
-        <meta charSet="utf-8" />
-        <title>Mouhamadou | Full-Stack Developer</title>
-      </Helmet>
-      <div className={HomeStyles.app}>
-        <div className={HomeStyles.appContainer}>
-          <Hero />
-          <About />
-          <Portfolio />
-          <MoreProjects />
-          <CTA />
-        </div>
+  const [loading, setLoading] = useState(true)
+  useEffect(() => {
+    setTimeout(() => {
+      setLoading(false)
+    }, 500)
+  }, [])
+
+  if (loading) {
+    return (
+      <div className={HomeStyles.ballsContainer}>
+        <img src={balls} alt="balls" className={HomeStyles.balls} />
       </div>
-    </Layout>
+    )
+  }
+
+  return (
+    <>
+      <FadeIn>
+        <Layout>
+          <Helmet>
+            <meta charSet="utf-8" />
+            <title>Mouhamadou | Full-Stack Developer</title>
+          </Helmet>
+          <div className={HomeStyles.app}>
+            <div className={HomeStyles.appContainer}>
+              <Hero />
+              <About />
+              <Portfolio />
+              <MoreProjects />
+              <CTA />
+            </div>
+          </div>
+        </Layout>
+      </FadeIn>
+    </>
   )
 }
